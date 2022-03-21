c03a671c3e11177b310a0abd9a7e845b3892ed71
Lucas Gomes <lucasgomesbt@gmail.com>
2022-03-20 22:43:45 -0300

    Adiçao do spinner para evitar visualizaçao de conteudo em pagina privada

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.scss";
+
+export function Loading() {
+    return (
+        <div className={styles.loading}></div>
+    )
+}
--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -2,11 +2,14 @@ import Router from "next/router";
 import { useEffect } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
+import { Loading } from "../Loading";
 
 const protectedRoute = (Component) => (props) => {
     const loggedIn = useSelector((state) => state.auth.loggedIn);
+    const user = useSelector((state) => state.auth.user);
+
     useEffect(() => {
-        if (!loggedIn) {
+        if (!loggedIn || user.email) {
             Router.push("/");
             toast.error("Para acessar essa página é necessário fazer login primeiro!");
         }
@@ -14,7 +17,7 @@ const protectedRoute = (Component) => (props) => {
     return (
         <>
             <Toaster />
-            <Component {...props} />
+            {!loggedIn ? <Loading /> : <Component {...props} />}
         </>
     );
 }
