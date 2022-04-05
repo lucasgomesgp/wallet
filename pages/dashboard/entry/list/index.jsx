86cef4335f000daa90c80a91cef06102569ce487
Lucas Gomes <lukinhas@pop-os.localdomain>
2022-04-05 17:01:26 -0300

    Adição da parte de listagem

--- pages/dashboard/entry/list/index.jsx
+++ pages/dashboard/entry/list/index.jsx
@@ -1,8 +1,8 @@
-
 import { Header } from "../../../../src/components/Header";
 import { Meta } from "../../../../src/components/Meta";
 import { Navbar } from "../../../../src/components/Navbar";
 import protectedRoute from "../../../../src/components/ProtectedRoute";
+import { ListItem } from "../../../../src/components/ListItem";
 import styles from "./styles.module.scss";
 
 function ListEntry() {
@@ -11,7 +11,20 @@ function ListEntry() {
       <Meta title="ListEntry" />
       <Header />
       <Navbar />
+      <table className={styles.container}>
+        <thead className={styles.title}>
+          <tr className={styles.line}>
+            <td>Data</td>
+            <td>Descrição</td>
+            <td>Valor</td>
+            <td>Ações</td>
+          </tr>
+        </thead>
+        <tbody>
+          <ListItem date="02/02/2018" description="Teste" value="R$2000" />
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
 export default protectedRoute(ListEntry);
